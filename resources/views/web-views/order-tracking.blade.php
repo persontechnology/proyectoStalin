d130bfe9f08b728b3bb88f64445468f8a77e930b
Freddy Lema <negritolema@gmail.com>
2021-07-08 15:43:21 -0500

    ´´pyamentlddddddl

--- resources/views/web-views/order-tracking.blade.php
+++ resources/views/web-views/order-tracking.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.front-end.app')
 
-@section('title','Seguir_Pedido)
+@section('title','Seguir_Pedido')
 
 @push('css_or_js')
     <meta property="og:image" content="{{asset('storage/app/public/company')}}/{{$web_config['web_logo']->value}}"/>
